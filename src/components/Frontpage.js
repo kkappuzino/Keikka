b2112ec4ec7d0874fb7329b97b26c6567c181dca
Paanila Venla Alisa <venla.paanila@edu.tampere.fi>
2019-12-17 14:50:31 +0200

    i busy

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import '../style/basic.scss';
-import Products from './Products';
-import pinimg from '../img/pin.jpg';
-import hodimg from '../img/hoodie.jpg';
-import shimg from '../img/shirt.jpg';
-import mimg from '../img/mug.jpg';
+import Gigs from './Gigs';
+ 
 
 
 
-const added = [{name:"", Q:""}];
+
 
 export default function Page() {
     return(
         <div className="container-fluid">
-            <Products products={products}/>
+            <Gigs/>
 
         </div>
 
--- src/components/Gigs.js
+++ src/components/Gigs.js
@@ -1,30 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import '../style/basic.scss';
-import CartButton from './CartButton';
+import '../App';
 
-const Box = (props) => {
+
+export default function Gigs (props){
+    const [gigs, setGigs] = useState([]) 
+    const [newGig, setNewGig] = useState('') 
+    const [showAll, setShowAll] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3000/gigs')
+            .then(response => {
+            setGigs(response.json)
+            })
+    }, [])
+
+    const gigsToShow = showAll
+    ? gigs
+    : gigs.filter(gig => {
+        return gig.tags.includes ='music';
+    });
+
+    const rows = () => gigsToShow.map(gig =>
+        <Gig/>
+    )
+
+
+    const handleGigChange = (event) => {
+        console.log(event.target.value)
+        setNewGig(event.target.value)
+    }
+
+    const addGig = (event) => {
+        event.preventDefault()
+        const gigObject = {
+            name: newGig,
+            date: new Date().toISOString(),
+            id: gigs.length + 1,
+        }
+
+        setGigs(gigs.concat(gigObject))
+        setNewGig('')
+    }
+
+    return (
+        <div className="row">
+            <div>
+            <button onClick={() => setShowAll(!showAll)}>
+                show {showAll ? 'tags' : 'all'}
+            </button>
+            </div>
+            {rows()}
+        </div>
+    )
+
+}
+
+
+const Gig = (props) => {
     return(
         <div className="col-3 col-sm-3 col-md-3 box" >
-            <img className=" py-5" src={props.product.img} alt={props.product.name} />
+            <img className=" py-5" src={props.gig.img} alt={props.gig.name}/>
             <div className="info pt-4">
-                <h2>{props.product.name}</h2>
-                <h3>{props.product.price}</h3>
+                <h2>{props.gig.name}</h2>
+                <h3>{props.gig.desc}</h3>
                 <br/>
-                <p>{props.product.color}</p>
-                <CartButton/>
+                <p>{props.gig.date}</p>
             </div>
         </div>
     )
 }
 
-
-export default function Products (props){
-    return(
-        <div className="row">
-            {props.products.map(c => (
-                <Box product = {c} key = {c.id}/>
-            ))}
-        </div>
-    )
-
-}
