7d7328985ecb12819d0b68ff571bb9cc7ee99dea
Paanila Venla Alisa <venla.paanila@edu.tampere.fi>
2020-01-20 16:01:28 +0200

    removia rakentamassa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React from 'react'
-import Frontpage from './components/Frontpage'
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import UpdateGigs from './components/UpdateGigs'
+import './style/basic.scss';
+import Gigs from './components/Gigs';
 
 const App = () => {
 
   return (
-    <div>
-      <Frontpage/>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/gigs">Update Gigs</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/gigs">
+            <UpdateGig />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
+}
+function Home() {
+  return (
+    <Gigs>
+      <h2>Home</h2>
+    </Gigs>
+  );
+   
+}
+
+function UpdateGig() {
+  return ( 
+  <UpdateGigs>
+    <h2>Update Gigs</h2>
+  </UpdateGigs>
+  );
 }
 
 export default App 
--- src/components/UpdateGigs.js
+++ src/components/UpdateGigs.js
@@ -6,7 +6,7 @@ import '../style/basic.scss';
 
 const UpdateGigs = ({UpdateGigs}) => {
     const [gigs, setGigs] = useState('') 
-    const [newGig, setNewGig] = useState('') 
+    const [newGig, setNewGig] = useState({name:'', desc:'', image:'',tags:''});
     const [newDesc, setNewDesc] = useState('') 
     const [newImg, setNewImg] = useState('') 
     const [newTags, setNewTags] = useState('') 
@@ -23,12 +23,11 @@ const UpdateGigs = ({UpdateGigs}) => {
     }, [])
 
 
-    const handleGigChange = (event) => {
+    const handleGigChange = (event, type) => {
         console.log(event.target.value)
-        setNewGig(event.target.value)
-        setNewDesc(event.target.value)
-        setNewImg(event.target.value)
-        setNewTags(event.target.value)
+        const tempGig = {...newGig};
+        tempGig[type] = event.target.value;
+        setNewGig(tempGig);
     }
 
     const addGig = (event) => {
@@ -52,25 +51,95 @@ const UpdateGigs = ({UpdateGigs}) => {
     }
 
     return(
-        <form onSubmit={addGig}>
-            <label>
-                Name:
-                <input type="text" placeholder="name" value={addGig.name} onChange={handleGigChange}/>
-            </label>
-            <label>
-                Description:
-                <input type="text" placeholder="description" value={addGig.desc} onChange={handleGigChange}/>
-            </label>
-            <label>
-                Image:
-                <input type="text" placeholder="image link" value={addGig.image} onChange={handleGigChange}/>
-            </label>
-            <label>
-                Tags :
-                <input type="text" placeholder="tag" value={addGig.tags} onChange={handleGigChange}/>
-            </label>
-            <input type="submit" value="Submit"/>
-         </form>
+        <div>
+            <form onSubmit={addGig}>
+                <label>
+                    Name:
+                    <input type="text" placeholder="name" value={addGig.name} onChange={e => handleGigChange(e, "name")}/>
+                </label>
+                <label>
+                    Description:
+                    <input type="text" placeholder="description" value={addGig.desc} onChange={e => handleGigChange(e, "desc")}/>
+                </label>
+                <label>
+                    Image:
+                    <input type="text" placeholder="image link" value={addGig.image} onChange={e => handleGigChange(e, "image")}/>
+                </label>
+                <label>
+                    Tags :
+                    <input type="text" placeholder="tag" value={addGig.tags} onChange={e => handleGigChange(e, "tags")}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+          
+         </div>
     )
+}
+
+
+const Gig = ({gig}) => {
+    const [removeGigs] = useState([]) 
+    gigService
+    .remove()
+    .then(removeGig =>{
+        console.log('effect')
+        console.log(removeGig)
+        removeGigs(removeGig)
+    }) 
+    const remove = id => {
+        tempGig = myGigs.filter(gig => gig.id != id);
+        setMyGig(tempGig);
+    }
+    return(
+    <div className="col-4 col-sm-4 col-md-4 box" >
+        <img className="py-5" src={gig.image} alt={gig.name}/>
+        <div className="pt-4 info">
+            <h2>{gig.name}</h2>
+            <h3>{gig.date}</h3>
+            <br/>
+            <p>{gig.desc}</p>
+            <p>{gig.tags}</p>
+
+            <button onSubmit={remove} type="submit"></button>
+        </div>
+    </div>
+    )
+}
+
+
+
+const Gigs = () => {
+    const [gigs, setGigs] = useState([]) 
+    const [showAll, setShowAll] = useState(true)
+
+    useEffect(() => {
+        
+        gigService
+            .getAll()
+            .then(allGigs =>{
+                console.log('effect')
+                console.log(allGigs)
+                setGigs(allGigs)
+            })
+    }, [])
+
+    const gigsToShow = showAll
+    ? gigs
+    : gigs.filter(gig => gig.tags.includes('music'))
+
+    return (
+        <div className="row">
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'tags' : 'all'}
+                </button>
+            </div>
+            <div>
+                {gigsToShow.map(gig => <Gig key={gig.id} gig={gig}/>)}
+            </div>
+
+        </div>
+    )
+
 }
 export default UpdateGigs;
